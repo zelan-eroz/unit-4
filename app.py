92705dfa269dd84cc2ed727a68889d8807edfaae
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-05-04 10:03:49 +0900

    Added working save feature, fixed log out,
    May 4th 10:03am

--- app.py
+++ app.py
@@ -107,25 +107,44 @@ def home():
 @app.route('/update', methods=['POST'])
 def update():
     if request.method=='POST':
-        num = request.form['num']
         post_id = request.form['post_id']
-        id=request.cookies.get('user_id')
-        db=database_worker('woof.db')
-        #CHECK IF USER ALREADY LIKED
-        liked = db.search(f"SELECT * FROM likes where uid='{id}' AND post_id={post_id}")
+        id = request.cookies.get('user_id')
+        db = database_worker('woof.db')
+        if request.form['submit'] == 'like':
+            print('like button clicked')
+            num = request.form['num']
+            # CHECK IF USER ALREADY LIKED
+            liked = db.search(f"SELECT * FROM likes where uid='{id}' AND post_id={post_id}")
 
-        #IF USER ALREADY LIKED, UNLIKE THE POST BY DECREASING NUM VALUE & DELETING ROW IN LIKES
-        if liked:
-            db.run_save(f"UPDATE posts set likes=likes-1 where id={post_id}")
-            db.run_save(f"DELETE from likes where uid='{id}' AND post_id={post_id}")
-        elif not liked:
-            db.run_save(f"UPDATE posts set likes=likes+1 where id={post_id}")
-            db.run_save(f"INSERT INTO likes(post_id,uid) VALUES({post_id},'{id}')")
+            # IF USER ALREADY LIKED, UNLIKE THE POST BY DECREASING NUM VALUE & DELETING ROW IN LIKES
+            if liked:
+                db.run_save(f"UPDATE posts set likes=likes-1 where id={post_id}")
+                db.run_save(f"DELETE from likes where uid='{id}' AND post_id={post_id}")
+            elif not liked:
+                db.run_save(f"UPDATE posts set likes=likes+1 where id={post_id}")
+                db.run_save(f"INSERT INTO likes(post_id,uid) VALUES({post_id},'{id}')")
 
-        #UPDATE HTML POSTS
-        posts = db.search(f"SELECT * from posts")
-        db.close()
-        return render_template('home.html', posts=posts)
+            # UPDATE HTML POSTS
+            posts = db.search(f"SELECT * from posts")
+            db.close()
+            return render_template('home.html', posts=posts)
+        elif request.form['submit']=='save':
+            print('save button clicked.')
+            posts = db.search(f"SELECT * from posts")
+
+            # CHECK IF USER ALREADY SAVED
+            saved = db.search(f"SELECT * FROM saves where uid='{id}' AND post_id={post_id}")
+            if saved:
+                db.run_save(f"DELETE from likes where uid='{id}' AND post_id={post_id}")
+                print("User already saved this.")
+            elif not saved:
+                print("Saving now")
+                db.run_save(f"INSERT INTO saves(post_id, uid) VALUES({post_id},'{id}')")
+            db.close()
+            return render_template('home.html', posts=posts)
+    else:
+        return render_template('/home')
+
 
 
 @app.route("/profile", methods=['GET','POST'])
@@ -142,6 +161,18 @@ def profile2():
         return render_template('profile.html', posts=posts)
     else:
         return redirect("/home")
+
+@app.route("/saves", methods=['GET'])
+def saves():
+    print("Now in saves")
+    id = request.cookies.get('user_id')
+    if id:
+        db = database_worker('woof.db')
+        posts = db.search(f"SELECT * FROM SAVES WHERE uid='{id}'")
+        db.close()
+        return render_template('saves.html',posts=posts)
+    else:
+        return redirect('/home')
 
 @app.route("/profile/<user_id>", methods=['GET','POST'])
 def profile(user_id:int):
@@ -180,7 +211,9 @@ def post():
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     resp = make_response(render_template('login.html'))
-    resp.set_cookie('user_id', "", expires=0) #delete cookie
-    return render_template('login.html')
+    resp.set_cookie('user_id', "", expires=0)  # delete cookie
+    return resp
+
+
 if __name__ == '__main__':
     app.run()
